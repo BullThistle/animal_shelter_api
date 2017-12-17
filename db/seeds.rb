7f6c0411e024f1a7a31c0e3012345928884c26c7
Rafael Furry <rfurry@gmail.com>
2017-12-17 06:46:16 -0800

    create seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Cat.destroy_all
+Dog.destroy_all
+
+cats = []
+dogs = []
+
+50.times do
+  cats.push(Cat.create!(
+    name: Faker::Zelda.character,
+    breed: Faker::Cat.breed,
+    sex: if rand(2) == 1
+      'Male'
+    else
+      'Female'
+    end
+  ))
+  dogs.push(Dog.create!(
+    name: Faker::Fallout.character,
+    breed: Faker::Dog.breed,
+    sex: if rand(2) == 1
+      'Male'
+    else
+      'Female'
+    end
+  ))
+end
+
+puts "Seeded #{Cat.count} cats and #{Dog.count} dogs."
