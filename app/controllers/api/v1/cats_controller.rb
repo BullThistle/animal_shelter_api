29b2cff493d91dd7229433367c228982ea8cf4ab
Rafael Furry <rfurry@gmail.com>
2017-12-17 06:01:03 -0800

    create CRUD for cats and dogs

--- app/controllers/api/v1/cats_controller.rb
+++ app/controllers/api/v1/cats_controller.rb
@@ -1,7 +1,31 @@
 class Api::V1::CatsController < ApplicationController
   
   def index
-    
+    @cats = Cat.all
+    json_response(@cats)
+  end
+  
+  def show
+    @cat = Cat.find(params[:id])
+    json_response(@cat)
+  end
+  
+  def create
+    @cat = Cat.create!(cat_params)
+    json_response(@cat, :created)
+  end
+  
+  def update
+    if @cat.update!(cat_params)
+      render status: 200, json: {
+        message: "This cat's info has been updated successfully."
+      }
+    end
+  end
+  
+  def destroy
+    @cat = Cat.find(params[:id])
+    @cat.destroy
   end
   
 private
--- app/controllers/api/v1/dogs_controller.rb
+++ app/controllers/api/v1/dogs_controller.rb
@@ -1,13 +1,37 @@
-class Api::V1::DogsController < ApplicationController
+Dogclass Api::V1::DogsController < ApplicationController
   
   def index
-    
+    @dogs = Dog.all
+    json_response(@dogs)
   end
   
-  private
-
-    def dog_params
-      params.permit(:name, :sex, :breed)
+  def show
+    @dog = Dog.find(params[:id])
+    json_response(@dog)
+  end
+  
+  def create
+    @dog = Dog.create!(dog_params)
+    json_response(@dog, :created)
+  end
+  
+  def update
+    if @dog.update!(dog_params)
+      render status: 200, json: {
+        message: "This dog's info has been updated successfully."
+      }
     end
-    
+  end
+  
+  def destroy
+    @dog = Dog.find(params[:id])
+    @dog.destroy
+  end
+  
+private
+
+  def dog_params
+    params.permit(:name, :sex, :breed)
+  end
+  
 end
